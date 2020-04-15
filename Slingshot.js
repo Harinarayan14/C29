682300573ba9eb9c5fa505fbe132710b2b6ac472
harinarayan14 <gjayashankar2008@gmail.com>
2020-04-15 17:12:26 +0530

    completed creating the rubber

--- Slingshot.js
+++ Slingshot.js
@@ -6,6 +6,10 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+        this.catapult1 = loadImage ("./sprites/sling1.png");
+        this.catapult2 = loadImage ("./sprites/sling2.png");
+        this.catapult3 = loadImage ("./sprites/sling3.png");
+
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,11 +20,17 @@ class SlingShot{
     }
 
     display(){
+        image(this.catapult1,200,20);
+        image(this.catapult2,170,20);
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            strokeWeight(8);
+            stroke(84,35,15);
+            line(pointA.x-25, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-25, pointA.y, pointB.x+30, pointB.y);
+            image(this.catapult3,pointA.x-25, pointA.y-10,15,30);
         }
     }
     
